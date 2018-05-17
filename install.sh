2ef287049ba0a09d0613d77a2e1ae8f81f3821fc
Howard Huang <hao@hwrd.me>
2018-05-17 15:46:11 -0700

    Add powertop service, split up user and system services

--- install.sh
+++ install.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-DEST_DIR="$HOME/.config/systemd/user"
+USER_DEST_DIR="$HOME/.config/systemd/user"
+SYS_DEST_DIR="/etc/systemd/system/"
 
 if [ $(uname) = "Linux" ]; then
-  mkdir -p "${DEST_DIR}"
+  mkdir -p "${USER_DEST_DIR}"
 
-  for file in "${PWD}/*.service"; do
-    cp -f $file "${DEST_DIR}"
+  for file in "${PWD}/user/*.service"; do
+    cp -f $file "${USER_DEST_DIR}"
   done
 
-  for file in "${PWD}/*.timer"; do
-    cp -f $file "${DEST_DIR}"
+  for file in "${PWD}/user/*.timer"; do
+    cp -f $file "${USER_DEST_DIR}"
+  done
+
+  for file in "${PWD}/system/*.service"; do
+    sudo cp -f $file "${SYS_DEST_DIR}"
   done
 
   systemctl --user daemon-reload
+  systemctl daemon-reload
   echo "Please create env file at ${HOME}/.systemd-env"
 fi
