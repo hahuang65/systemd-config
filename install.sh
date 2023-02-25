3306f376be7b7dac3da7fede05970918008265dc
Howard Huang <hao@hwrd.me>
2023-02-25 15:08:26 -0800

    Remove user-specific installation

--- install.sh
+++ install.sh
@@ -6,14 +6,6 @@ SYS_DEST_DIR="/etc/systemd/system/"
 if [ $(uname) = "Linux" ]; then
 	mkdir -p "${USER_DEST_DIR}"
 
-	for file in "${PWD}/user/*.service"; do
-		cp -f $file "${USER_DEST_DIR}"
-	done
-
-	for file in "${PWD}/user/*.timer"; do
-		cp -f $file "${USER_DEST_DIR}"
-	done
-
 	for file in "${PWD}/system/*.service"; do
 		sudo cp -f $file "${SYS_DEST_DIR}"
 	done
